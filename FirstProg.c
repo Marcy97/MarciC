955278b82fca97148b96989512bc1e9ef640efc1
Alessio Gianni Passoni <AlessioGPassoni@gmail.com>
2016-12-08 23:37:50 +0100

    Add files via upload

---
+++ FirstProg.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/*int somma(int a, int b){
+	return a+b;
+}*/
+
+int main(){
+int n,i,somma=0;
+for(i=0;i<10;i++)
+	{
+		printf("Dammi il numero: \n");
+		scanf("%i",&n);
+		somma=somma+n;
+	}
+
+//printf("Mi hai dato il numero: %i\n",n);
+}
+
