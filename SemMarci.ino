568c8ea49309d869e960725ab61ed53cd6391ca2
Alessio Gianni Passoni <AlessioGPassoni@gmail.com>
2016-12-30 18:49:42 +0100

    Add files via upload

---
+++ SemMarci.ino
@@ -0,0 +1,49 @@
+int ledG=11;
+int ledY=12;
+int ledR=13;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(ledG,OUTPUT);
+pinMode(ledY,OUTPUT);
+pinMode(ledR,OUTPUT);
+giornaliera();
+notturna();
+loop();
+}
+
+void giornaliera()
+{
+  int i;
+  for(i=0;i<3;i++)
+    {  
+      digitalWrite(ledG,HIGH);
+      delay(1000);
+      digitalWrite(ledY,HIGH);
+      delay(1000);
+      digitalWrite(ledG,LOW);
+      digitalWrite(ledY,LOW);
+      digitalWrite(ledR,HIGH);
+      delay(1000);
+      digitalWrite(ledR,LOW);
+    } 
+  
+}
+
+void notturna()
+{
+  int i;
+  for(i=0;i<9;i++)
+    {  
+      digitalWrite(ledY,HIGH);
+      delay(1000);
+      digitalWrite(ledY,LOW);
+      delay(1000);
+    } 
+  
+}
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+giornaliera();
+}
